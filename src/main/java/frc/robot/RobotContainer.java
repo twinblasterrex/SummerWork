b0c2d478ab14e96c5580d78b4e85bc45a3ddb51b
Matthew Lomando <80913065+twinblasterrex@users.noreply.github.com>
2023-08-16 13:10:35 -0400

    Initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer
+{
+    public RobotContainer()
+    {
+        // Configure the trigger bindings
+        configureBindings();
+    }
+    
+    
+    /** Use this method to define your trigger->command mappings. */
+    private void configureBindings()
+    {
+
+    }
+    
+    
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand()
+    {
+        // TODO: Implement properly
+        return null;
+    }
+}
